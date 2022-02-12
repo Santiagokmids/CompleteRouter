bb86d23acd27c47b7405bca26c8f564c650e5ef6
SANTIAGO TROCHEZ VELASCO <1193573597@u.icesi.edu.co>
2022-02-11 21:02:06 -0500

    New let about has been added

--- Router.js
+++ Router.js
@@ -66,3 +66,24 @@ let About = /*html*/`
         `
 
 export default About;
+
+let About = {
+    render : async () => {
+        let view = /*html*/`
+        <section class = "section">
+            <h1> About </h1>
+            <button id = "myBtn"> Button </button>
+        </section>
+    `
+
+    return view
+    },
+    after_render : async () => {
+        document.getElementById("myBtn").addEventListener("click", () => {  
+            console.log('Yo')
+            alert('Yo')
+        })
+    }
+}
+
+export default About;
